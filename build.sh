118c05d1ed4e762840787fad4562cf649af7fb8d
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-11-06 12:09:11 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ g++ c1-aes256/aes256.cpp -o bin/c1
 echo "building binaries for the second cipher layer: c2-CAMELLIA-POLY..."
 echo ""
 apt-get -y install build-essential libssl-dev
-gcc ./c2-CAMELLIA-POLY/cryptolandi.c -o ../bin/c2  -lssl -lcrypto
-cp bin/c3 /usr/bin/
+gcc ./c2-CAMELLIA-POLY/cryptolandi.c -o bin/c2  -lssl -lcrypto
+cp /bin/c2 /usr/bin/
 
 echo "building binaries for the third cipher layer: c3-NTRUprime..."
 echo ""
