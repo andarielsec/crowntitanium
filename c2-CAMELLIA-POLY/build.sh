fc368292cd47f734ff6a53bb8a390f1748f4ad5e
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2023-11-14 12:29:03 +0100

    Update build.sh

--- c2-CAMELLIA-POLY/build.sh
+++ c2-CAMELLIA-POLY/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #install required libraries on Debian 10
 apt-get -y install build-essential libssl-dev
-gcc cryptolandi.c -o bin/c3  -lssl -lcrypto
-cp bin/c3 /usr/bin/
+gcc cryptolandi.c -o bin/c2  -lssl -lcrypto
+cp bin/c2 /usr/bin/
 	
